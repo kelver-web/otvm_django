98486c83cfdef3bfead8c76aa35ebda825da05af
kelver-web <kelverwt@gmail.com>
2024-02-02 18:22:05 -0300

    get_list_or_404

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 from utils.recipes.factory import make_recipe
 
 from .models import Recipe
@@ -14,17 +14,32 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id,
-        is_published=True
-    ).order_by('-id')
+    # recipes = Recipe.objects.filter(
+    #     category__id=category_id,
+    #     is_published=True
+    # ).order_by('-id')
 
-    if not recipes:
-        return render(request, 'recipes/erros/404.html')
+    # if not recipes:
+    #     return render(request, 'recipes/erros/404.html')
     
+    recipes = get_list_or_404(
+        Recipe.objects.filter(category__id=category_id,
+                              is_published=True).order_by('-id'))
+    
+    # return render(request, 'recipes/pages/category.html', context={
+    #     'recipes': recipes,
+    #     'title': f'{recipes.first().category.name} - Category | '
+    # })
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-        'title': f'{recipes.first().category.name} - Category | '
+        'title': f'{recipes[0].category.name} - Category | '
+    })
+
+
+def search(request):
+    return render(request,'recipes/pages/search.html', context={
+      'recipes': Recipe.objects.filter(is_published=True),
     })
 
 
